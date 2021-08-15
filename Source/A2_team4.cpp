4a1410d656604bdd28ad6af99ac685083c78daed
Ceres <donovano@email.com>
2021-08-15 07:07:51 -0400

    Quick Tweak to reorient skybox
    Face the more interesting side towards the default camera position

--- Source/A2_team4.cpp
+++ Source/A2_team4.cpp
@@ -1053,6 +1053,7 @@ void drawSkybox(Shader shader)
 	glm::mat4 model = glm::mat4(1.0f);
 
 	model = glm::translate(model, glm::vec3(0.0f, 0.0f, -1.0f));
+	model = glm::rotate(model, glm::radians(180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
 	model = glm::scale(model, glm::vec3(20.0f, 20.0f, 20.0f));
 	
 	shader.setMat4("model", model);
